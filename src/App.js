73b8d3b6156b6c94e6b575ee1a3a7e8c3fb39cf2
satya M <satya2maity@gmail.com>
2022-12-14 21:35:43 +0530

    added new

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
-import './App.css';
-import Footer from './Components/Pages/Footer';
-import Home from './Components/Pages/Home';
-import Navbar from './Components/Pages/Navbar';
+import "./App.css";
+import Footer from "./Components/Pages/Footer";
+import Home from "./Components/Pages/Home";
+import Navbar from "./Components/Pages/Navbar";
+import Login from "./Components/AuthPages/Login";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <hr/>
-    <Home/>
-    <hr/>
-    <Footer/>
+      <Login />
+      <Navbar />
+      <hr />
+      <Home />
+      <hr />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <Auth0Provider
+    domain="dev-pdzdss8p3vtolnn1.us.auth0.com"
+    clientId="Ogju5GoD0TPgb64x7FrXpgoEidGiu8f8"
+    redirectUri={window.location.origin}
+  >
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
